882b56b4edc7bc62a12c2e079c1d22c0394be1af
Bruno Gonçalves <bruno.goncalves@synvia.com>
2020-04-20 13:34:19 -0300

    heroku deploy

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname + '/index.html'));
+app.use(express.static(__dirname + '/dist/venturus-sports'));
+
+router.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/dist/venturus-sports/index.html'));
 });
 
-app.use('/', router);
 app.listen(process.env.PORT || 8000);
